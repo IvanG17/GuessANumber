b724a52df7acdd106cabded0c3645e4a82133de5
Ivan <ivang12@g.ucla.edu>
2021-08-15 15:27:51 -0700

    guessing implementation

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 'use strict';
 
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Guess!";
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Correct Guess!";
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 21;
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 21;
 
-console.log(document.querySelector(".guess").value);
-document
+// console.log(document.querySelector(".guess").value);
+// document
+
+const secretNumber = Math.trunc(Math.random() * 25 + 1);
+document.querySelector(".number").textContent = secretNumber;
+
+function userClick() {
+    let userGuess = Number(document.querySelector(".guess").value);
+    if (!userGuess) {
+        document.querySelector(".message").textContent = "No number!";
+        wrongGuess();
+    } else if (userGuess === secretNumber) {
+        document.querySelector(".message").textContent = "Correct Number- Congratulations!"
+    } else if (userGuess > secretNumber) {
+        document.querySelector(".message").textContent = "Guess too high"
+        wrongGuess();
+    } else if (userGuess < secretNumber) {
+        document.querySelector(".message").textContent = "Guess too low"
+        wrongGuess();
+    }
+}
+
+function wrongGuess() {
+    let score = Number(document.querySelector(".score").textContent);
+    score -= 1;
+    document.querySelector(".score").textContent = score.toString();
+}
+
+
+document.querySelector('.check').addEventListener('click', userClick);
