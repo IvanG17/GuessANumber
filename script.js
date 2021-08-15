e30e928668f7240a3fd7f8afcac0f9c8ee6b4fb2
Ivan <ivang12@g.ucla.edu>
2021-08-15 14:55:19 -0700

    text Contents

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "Correct Guess!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 21;
+
+console.log(document.querySelector(".guess").value);
+document
